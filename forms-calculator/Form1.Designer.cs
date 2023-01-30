ada98000a5c6c7a9644f2032e3c381a302e21374
Mateusz Drążkiewicz <matekudr@gmail.com>
2023-01-30 18:16:12 +0100

    change form style and add buttons funcion

--- forms-calculator/Form1.Designer.cs
+++ forms-calculator/Form1.Designer.cs
@@ -28,19 +28,228 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.one = new System.Windows.Forms.Button();
+            this.two = new System.Windows.Forms.Button();
+            this.three = new System.Windows.Forms.Button();
+            this.minus = new System.Windows.Forms.Button();
+            this.four = new System.Windows.Forms.Button();
+            this.five = new System.Windows.Forms.Button();
+            this.six = new System.Windows.Forms.Button();
+            this.plus = new System.Windows.Forms.Button();
+            this.seven = new System.Windows.Forms.Button();
+            this.eight = new System.Windows.Forms.Button();
+            this.nine = new System.Windows.Forms.Button();
+            this.suma = new System.Windows.Forms.Button();
+            this.dz = new System.Windows.Forms.Button();
+            this.zero = new System.Windows.Forms.Button();
+            this.mn = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(160, 23);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // one
+            // 
+            this.one.Location = new System.Drawing.Point(12, 56);
+            this.one.Name = "one";
+            this.one.Size = new System.Drawing.Size(35, 23);
+            this.one.TabIndex = 1;
+            this.one.Text = "1";
+            this.one.UseVisualStyleBackColor = true;
+            this.one.Click += new System.EventHandler(this.one_Click);
+            // 
+            // two
+            // 
+            this.two.Location = new System.Drawing.Point(53, 56);
+            this.two.Name = "two";
+            this.two.Size = new System.Drawing.Size(35, 23);
+            this.two.TabIndex = 2;
+            this.two.Text = "2";
+            this.two.UseVisualStyleBackColor = true;
+            this.two.Click += new System.EventHandler(this.two_Click);
+            // 
+            // three
+            // 
+            this.three.Location = new System.Drawing.Point(94, 56);
+            this.three.Name = "three";
+            this.three.Size = new System.Drawing.Size(35, 23);
+            this.three.TabIndex = 3;
+            this.three.Text = "3";
+            this.three.UseVisualStyleBackColor = true;
+            this.three.Click += new System.EventHandler(this.three_Click);
+            // 
+            // minus
+            // 
+            this.minus.Location = new System.Drawing.Point(135, 56);
+            this.minus.Name = "minus";
+            this.minus.Size = new System.Drawing.Size(35, 23);
+            this.minus.TabIndex = 4;
+            this.minus.Text = "-";
+            this.minus.UseVisualStyleBackColor = true;
+            this.minus.Click += new System.EventHandler(this.minus_Click);
+            // 
+            // four
+            // 
+            this.four.Location = new System.Drawing.Point(12, 85);
+            this.four.Name = "four";
+            this.four.Size = new System.Drawing.Size(35, 23);
+            this.four.TabIndex = 5;
+            this.four.Text = "4";
+            this.four.UseVisualStyleBackColor = true;
+            this.four.Click += new System.EventHandler(this.four_Click);
+            // 
+            // five
+            // 
+            this.five.Location = new System.Drawing.Point(53, 85);
+            this.five.Name = "five";
+            this.five.Size = new System.Drawing.Size(35, 23);
+            this.five.TabIndex = 6;
+            this.five.Text = "5";
+            this.five.UseVisualStyleBackColor = true;
+            this.five.Click += new System.EventHandler(this.five_Click);
+            // 
+            // six
+            // 
+            this.six.Location = new System.Drawing.Point(94, 85);
+            this.six.Name = "six";
+            this.six.Size = new System.Drawing.Size(35, 23);
+            this.six.TabIndex = 7;
+            this.six.Text = "6";
+            this.six.UseVisualStyleBackColor = true;
+            this.six.Click += new System.EventHandler(this.six_Click);
+            // 
+            // plus
+            // 
+            this.plus.Location = new System.Drawing.Point(135, 85);
+            this.plus.Name = "plus";
+            this.plus.Size = new System.Drawing.Size(35, 23);
+            this.plus.TabIndex = 8;
+            this.plus.Text = "+";
+            this.plus.UseVisualStyleBackColor = true;
+            this.plus.Click += new System.EventHandler(this.plus_Click);
+            // 
+            // seven
+            // 
+            this.seven.Location = new System.Drawing.Point(12, 114);
+            this.seven.Name = "seven";
+            this.seven.Size = new System.Drawing.Size(35, 23);
+            this.seven.TabIndex = 9;
+            this.seven.Text = "7";
+            this.seven.UseVisualStyleBackColor = true;
+            this.seven.Click += new System.EventHandler(this.seven_Click);
+            // 
+            // eight
+            // 
+            this.eight.Location = new System.Drawing.Point(53, 114);
+            this.eight.Name = "eight";
+            this.eight.Size = new System.Drawing.Size(35, 23);
+            this.eight.TabIndex = 10;
+            this.eight.Text = "8";
+            this.eight.UseVisualStyleBackColor = true;
+            this.eight.Click += new System.EventHandler(this.eight_Click);
+            // 
+            // nine
+            // 
+            this.nine.Location = new System.Drawing.Point(94, 114);
+            this.nine.Name = "nine";
+            this.nine.Size = new System.Drawing.Size(35, 23);
+            this.nine.TabIndex = 11;
+            this.nine.Text = "9";
+            this.nine.UseVisualStyleBackColor = true;
+            this.nine.Click += new System.EventHandler(this.nine_Click);
+            // 
+            // suma
+            // 
+            this.suma.Location = new System.Drawing.Point(135, 114);
+            this.suma.Name = "suma";
+            this.suma.Size = new System.Drawing.Size(35, 50);
+            this.suma.TabIndex = 12;
+            this.suma.Text = "=";
+            this.suma.UseVisualStyleBackColor = true;
+            this.suma.Click += new System.EventHandler(this.suma_Click);
+            // 
+            // dz
+            // 
+            this.dz.Location = new System.Drawing.Point(12, 141);
+            this.dz.Name = "dz";
+            this.dz.Size = new System.Drawing.Size(35, 23);
+            this.dz.TabIndex = 13;
+            this.dz.Text = "/";
+            this.dz.UseVisualStyleBackColor = true;
+            this.dz.Click += new System.EventHandler(this.dz_Click);
+            // 
+            // zero
+            // 
+            this.zero.Location = new System.Drawing.Point(53, 141);
+            this.zero.Name = "zero";
+            this.zero.Size = new System.Drawing.Size(35, 23);
+            this.zero.TabIndex = 14;
+            this.zero.Text = "0";
+            this.zero.UseVisualStyleBackColor = true;
+            this.zero.Click += new System.EventHandler(this.zero_Click);
+            // 
+            // mn
+            // 
+            this.mn.Location = new System.Drawing.Point(94, 141);
+            this.mn.Name = "mn";
+            this.mn.Size = new System.Drawing.Size(35, 23);
+            this.mn.TabIndex = 15;
+            this.mn.Text = "*";
+            this.mn.UseVisualStyleBackColor = true;
+            this.mn.Click += new System.EventHandler(this.mn_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(894, 426);
+            this.ClientSize = new System.Drawing.Size(192, 180);
+            this.Controls.Add(this.mn);
+            this.Controls.Add(this.zero);
+            this.Controls.Add(this.dz);
+            this.Controls.Add(this.suma);
+            this.Controls.Add(this.nine);
+            this.Controls.Add(this.eight);
+            this.Controls.Add(this.seven);
+            this.Controls.Add(this.plus);
+            this.Controls.Add(this.six);
+            this.Controls.Add(this.five);
+            this.Controls.Add(this.four);
+            this.Controls.Add(this.minus);
+            this.Controls.Add(this.three);
+            this.Controls.Add(this.two);
+            this.Controls.Add(this.one);
+            this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private TextBox textBox1;
+        private Button one;
+        private Button two;
+        private Button three;
+        private Button minus;
+        private Button four;
+        private Button five;
+        private Button six;
+        private Button plus;
+        private Button seven;
+        private Button eight;
+        private Button nine;
+        private Button suma;
+        private Button dz;
+        private Button zero;
+        private Button mn;
     }
 }
--- forms-calculator/Form1.cs
+++ forms-calculator/Form1.cs
@@ -6,5 +6,90 @@ namespace forms_calculator
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void one_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void two_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void three_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void minus_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void four_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void five_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void six_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void plus_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void seven_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void eight_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void nine_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void suma_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void zero_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dz_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
